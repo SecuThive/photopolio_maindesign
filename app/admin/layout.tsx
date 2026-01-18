f803a9c789d531cd9e9c590c2ca446881fa8b9f9
Devthive <thive8564@gmail.com>
2026-01-19 02:12:38 +0900

    update seo2

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,3 +7,11 @@ export const metadata: Metadata = {
     follow: false,
   },
 }
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
